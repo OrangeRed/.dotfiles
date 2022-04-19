d5db89bf82cc3c60cf50b21c4a7f19a2b5596c67
Dmitriy Kagno <kagno.dmitriy@gmail.com>
2022-04-19 18:30:00 -0400

    Made fish prompt slightly better

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,10 +1,14 @@
 function fish_prompt
+    echo -n \n(set_color 808080)"╭─"
     echo -n (set_color yellow)"$USER"
     echo -n (set_color white)": "
     echo -n (set_color blue)(prompt_pwd)(set_color white)
 
     if not set -q __fish_git_prompt_show_informative_status
         set -g __fish_git_prompt_show_informative_status 1
+    end
+    if not set -q __fish_git_prompt_showupstream
+        set -g __fish_git_prompt_showupstream none
     end
     if not set -q __fish_git_prompt_char_stateseparator 
         set -g __fish_git_prompt_char_stateseparator "|"
@@ -13,9 +17,6 @@ function fish_prompt
         set -g __fish_git_prompt_color_branch magenta --bold
         set -g __fish_git_prompt_char_cleanstate ""
     end
-#    if not set -q __fish_git_prompt_showupstream
-#        set -g __fish_git_prompt_showupstream informative
-#    end
     if not set -q __fish_git_prompt_color_dirtystate
         set -g __fish_git_prompt_color_dirtystate yellow
     end
@@ -35,6 +36,7 @@ function fish_prompt
     # printf '%s ' (fish_vcs_prompt | sed 's/\([1-9]\+\)/\1 /g')
     echo -n (fish_vcs_prompt)
 
-    echo -n \n' '(set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
+    echo -n \n(set_color 808080)"╰─"
+    echo -n (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
     set_color normal
 end
