04ab43537c6818e1a00bc3519f2203df485b34a5
Dmitriy Kagno <kagno.dmitriy@gmail.com>
2022-04-20 17:58:30 -0400

    remove tree view from l.fish

--- fish/functions/l.fish
+++ fish/functions/l.fish
@@ -1,4 +1,4 @@
 function l --wraps=clear --description 'alias exa'
-  exa --all --tree --level=1 $argv --icons --group-directories-first --ignore-glob="node_modules|.git" --git-ignore;
+  exa --all $argv --icons --group-directories-first --ignore-glob="node_modules|.git" --git-ignore;
 end
 
