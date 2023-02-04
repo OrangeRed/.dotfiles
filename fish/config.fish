f74c0fb6bb391af8c03b6dd724e459107e4bbeb0
Dmitriy <kagno.dmitriy@gmail.com>
2023-02-03 23:56:42 -0500

    Add more colors to EXA

--- fish/config.fish
+++ fish/config.fish
@@ -22,13 +22,15 @@ end
 
 ## EXA COLORS
 # consider loading this on session start instead
-set -Ux EXA_COLORS (echo "
+set -gx EXA_COLORS (echo "
 da=38;5;245:
 di=38;5;245:
 *.*sh=32:
 *.*css*=38;5;159:
-*.js*=33:
+*.*js*=33:
 *.ts*=34:
-*.yml=31
+*.yml=31:
+*.yaml=31:
+*.astro=31:
 " | tr -d '\n')
 # END EXA COLORS
