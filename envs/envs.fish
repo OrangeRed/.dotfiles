8d0da05d644c267c94552d7b4dc5141fab971602
Dmitriy <kagno.dmitriy@gmail.com>
2023-06-07 19:41:44 -0400

    chore: move envs into their own folder

--- envs/envs.fish
+++ envs/envs.fish
@@ -1,4 +1,7 @@
-## EXA COLORS
+# Manually source these envs if you don't want to reboot
+
+set -Ux MOZ_ENABLE_WAYLAND "1 firefox"
+
 set -Ux EXA_COLORS "\
 da=38;5;245:\
 di=38;5;245:\
@@ -9,13 +12,15 @@ di=38;5;245:\
 *.yml=31:\
 *.yaml=31:\
 *.astro=31:"
-# END EXA COLORS
 
-## FZF DEFAULT OPTS
 set -Ux FZF_DEFAULT_OPTS "\
--i --cycle --height "95%" \
+-i --cycle --height '95%' \
 --layout reverse --no-scrollbar --prompt '  ' \
 --color=bg+:#313244,bg:#1e1e2e,spinner:#f38ba8,hl:#f38ba8 \
 --color=fg:#cdd6f4,header:#f38ba8,info:yellow,pointer:#f38ba8 \
 --color=marker:#f38ba8,fg+:#cdd6f4,prompt:#f38ba8,hl+:#f38ba8"
-# END FZF DEFAULT OPTS
+
+set -Ux GTK_IM_MODULE "fcitx"
+set -Ux QT_IM_MODULE "fcitx"
+set -Ux SDL_IM_MODULE "fcitx"
+set -Ux XMODIFIERS "@im=fcitx"
