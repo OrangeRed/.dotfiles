07d129b4a89a9137bbeba6af48dd25bec60f1fac
Dmitriy <kagno.dmitriy@gmail.com>
2025-08-14 12:12:42 -0400

    change fcitx5 envs

--- fish/.config/fish/conf.d/envs.fish
+++ fish/.config/fish/conf.d/envs.fish
@@ -4,26 +4,25 @@ if status is-login
 
     set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
 
-    set -gx EXA_COLORS (string replace -r -a '\s+' '' "\
-        da=38;5;245:\
-        di=38;5;245:\
-        *.*sh=32:\
-        *.*css*=38;5;159:\
-        *.*js*=33:\
-        *.ts*=34:\
-        *.yml=31:\
-        *.yaml=31:\
-        *.astro=31:")
+    set -gx EXA_COLORS (string join "" \
+        "da=38;5;245:" \
+        "di=38;5;245:" \
+        "*.*sh=32:" \
+        "*.*css*=38;5;159:" \
+        "*.*js*=33:" \
+        "*.ts*=34:" \
+        "*.yml=31:" \
+        "*.yaml=31:" \
+        "*.astro=31")
 
-    set -gx FZF_DEFAULT_OPTS (string replace -r -a '\s{3,}' ' ' "\
-        -i --cycle --height '95%' \
-        --layout reverse --prompt '  ' \
-        --color=bg+:#313244,bg:#1e1e2e,spinner:#f38ba8,hl:#f38ba8 \
-        --color=fg:#cdd6f4,header:#f38ba8,info:yellow,pointer:#f38ba8 \
-        --color=marker:#f38ba8,fg+:#cdd6f4,prompt:#f38ba8,hl+:#f38ba8")
+    set -gx FZF_DEFAULT_OPTS (string join "" \
+        " -i --cycle --height '95%' --layout reverse --prompt '  '" \
+        " --color=bg+:#313244,bg:#1e1e2e,spinner:#f38ba8,hl:#f38ba8" \
+        " --color=fg:#cdd6f4,header:#f38ba8,info:yellow,pointer:#f38ba8" \
+        " --color=marker:#f38ba8,fg+:#cdd6f4,prompt:#f38ba8,hl+:#f38ba8")
 
-    set -gx GTK_IM_MODULE fcitx5
-    set -gx QT_IM_MODULE fcitx5
-    set -gx SDL_IM_MODULE fcitx5
-    set -gx XMODIFIERS "@im=fcitx5"
+    # set -gx GTK_IM_MODULE fcitx5
+    # set -gx QT_IM_MODULE fcitx5
+    # set -gx SDL_IM_MODULE fcitx5
+    # set -gx XMODIFIERS "@im=fcitx5"
 end
